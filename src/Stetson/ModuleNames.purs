bbf8e4b87a8d51b3469c03033b45de7d80b77091
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2019-04-11 10:10:45 +0000

    Update psc-package.json

--- src/Stetson/ModuleNames.purs
+++ src/Stetson/ModuleNames.purs
@@ -2,11 +2,11 @@ module Stetson.ModuleNames where
 
 import Erl.ModuleName (ModuleName(..))
 
+stetsonModuleNames :: ModuleName
+stetsonModuleNames = ModuleName "Stetson.ModuleNames"
+
 stetsonRest :: ModuleName
 stetsonRest = ModuleName "Stetson.Rest"
 
 stetsonRestHandler :: ModuleName
 stetsonRestHandler = ModuleName "Stetson.RestHandler"
-
-stetsonModuleNames :: ModuleName
-stetsonModuleNames = ModuleName "Stetson.ModuleNames"
